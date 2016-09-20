8d92a3a9a21e048960aa14b8fbcb4f018e0c8317
Adam Dittebrand <Adam Dittebrand>
2016-09-20 15:44:44 -0400

    working toward getting 3 favorite numbers. (inputting 3 different parameters

--- personal_app.rb
+++ personal_app.rb
@@ -13,3 +13,17 @@ get '/age' do
 	name = params[:users_name].capitalize
 	erb :get_age, :locals => {:name => name}
 end
+
+post '/age' do
+	age = params[:age]
+	name = params[:retrieved_name]
+	redirect '/fav_numbers?name=' + name +"&age=" + age
+end
+
+get '/fav_numbers' do
+	numbers = params[:fav_nums]
+	erb :favorite_numbers, :locals => {:name => name, :age => age}
+end
+
+
+
